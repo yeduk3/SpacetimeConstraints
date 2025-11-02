7836dc883c497dd44d26017a14eed57dba021ee9
yeduk3 <yeduk33@gmail.com>
2025-11-02 17:17:25 +0900

    Initial Commit

---
+++ SpacetimeConstraints/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SpacetimeConstraints
+//
+//  Created by 이용규 on 11/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
